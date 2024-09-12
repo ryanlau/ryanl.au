e8a14ed13a0ce07430374c4383ffade5f27de82a
Ryan Lau <47727459+ryanlau@users.noreply.github.com>
2024-09-12 18:33:46 -0400

    style: setup tailwind prettier plugin

--- src/collections/Projects.ts
+++ src/collections/Projects.ts
@@ -41,6 +41,13 @@ export const Projects: CollectionConfig = {
         description: 'Used solely for ordering',
       },
       required: true,
+      defaultValue: () => {
+        const now = new Date()
+        const year = now.getUTCFullYear()
+        const month = String(now.getUTCMonth() + 1).padStart(2, '0')
+
+        return `${year}-${month}-01T12:00:00.000Z`
+      },
     },
     lexicalHTML('title', { name: 'title_html' }),
     lexicalHTML('description', { name: 'description_html' }),
